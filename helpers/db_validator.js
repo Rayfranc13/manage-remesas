abc7a5fbf2021c651ec452303f168a74b0320a1a
Ray <rayfranc13@gmail.com>
2022-01-17 16:01:51 -0500

    Implementando DB

--- helpers/db_validator.js
+++ helpers/db_validator.js
@@ -1,6 +1,6 @@
 const {Usuario}=require('../models/usuario')
 
-const existeCorreo=(correo='')=>{
+const existeCorreo=async(correo='')=>{
 const existe= await Usuario.findOne({correo})
 if(!existe){
     throw new Error('El correo ya esta registrado')
