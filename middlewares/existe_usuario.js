17c7278b1460c75a9df7fb2a9c81e45b4b761970
Ray <rayfranc13@gmail.com>
2022-01-27 16:29:23 -0500

    LogIn

--- middlewares/existe_usuario.js
+++ middlewares/existe_usuario.js
@@ -4,7 +4,7 @@ const Usuario=require('../models/usuario')
 
 const existUserByEmail= async(req,res,next)=>{
 const {correo,password}=req.body
-const usuario= await Usuario.findOne({correo})
+const usuario= await Usuario.findOne({correo}).select('password')
 if(!usuario){
     return res.status(400).json({
         message:'El correo o el password son incorrectos',
