fcf560b1c89b68613d12c9b5a9d346b871801432
Ray <rayfranc13@gmail.com>
2022-01-27 23:44:57 -0500

    LogIn

--- middlewares/validar_propiedad.js
+++ middlewares/validar_propiedad.js
@@ -5,15 +5,15 @@ const propiedad=(req,res,next)=>{
   const usuario=req.usuario
   const beneficiario= Beneficiario.findById(id)
   
-  if(beneficiario.usuario_id===usuario._id){
-      return next()
+  if(!(beneficiario.usuario_id===usuario._id)){
+    return res.status(401).json({
+        message:'No esta autorizado para esta operacion',
+        beneficiario,
+        usuario
+    })
   } 
-
-  return res.status(401).json({
-      message:'No esta autorizado para esta operacion',
-      beneficiario,
-      usuario
-  })
+ return next()
+ 
 
 }
 
