94a588b748ecede407073d2fda8bac6835b7eda8
Ray <rayfranc13@gmail.com>
2022-01-27 20:18:12 -0500

    LogIn

--- controllers/beneficiario.js
+++ controllers/beneficiario.js
@@ -4,7 +4,7 @@ const Beneficiario=require('../models/beneficiario')
 const getBeneficiarios=async(req=request,res)=>{
     const {id}=req.payload
 
-    const beneficiarios=await Beneficiario.find({usuario_id=id})
+    const beneficiarios=await Beneficiario.find({usuario_id:id})
 res.json({
     beneficiarios
 })
