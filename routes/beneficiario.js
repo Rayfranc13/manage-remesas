0e226e60a40cec3d5885c387308db21d5bfb7c18
Ray <rayfranc13@gmail.com>
2022-01-27 19:53:07 -0500

    LogIn

--- routes/beneficiario.js
+++ routes/beneficiario.js
@@ -1,9 +1,12 @@
 const {Router}=require('express');
 const {check}=require('express-validator');
 const { validarToken } = require('../middlewares/validarToken');
+const {getBeneficiarios}=require('../controllers/beneficiario')
 const router=Router()
 
 
 router.get("/",[
-    validarToken,
+    validarToken
 ],getBeneficiarios)
+
+module.exports=router
