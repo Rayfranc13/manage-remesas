1d10817100748267533d8dbb439c57f5f24b9692
Ray <rayfranc13@gmail.com>
2022-01-27 20:37:00 -0500

    LogIn

--- routes/beneficiario.js
+++ routes/beneficiario.js
@@ -13,7 +13,6 @@ router.get("/",[
 router.post('/',[
     check('nombre','El nombre no puede estar vacio').notEmpty(),
     check('nombre','El nombre no puede ser un numero').isString(),
-    check('password','El password deve incluir mayusculas,numeros y caracteres especiales').isStrongPassword(),
     check('tarjeta','La tarjeta no es valida').isLength(16),
     validarCampos,
     validarToken
