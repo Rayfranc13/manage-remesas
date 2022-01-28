c511a854ead6bbf64851af9b17fcbd823d5143fe
Ray <rayfranc13@gmail.com>
2022-01-27 20:34:25 -0500

    LogIn

--- routes/beneficiario.js
+++ routes/beneficiario.js
@@ -1,7 +1,8 @@
 const {Router}=require('express');
 const {check}=require('express-validator');
 const { validarToken } = require('../middlewares/validarToken');
-const {getBeneficiarios, postBeneficiario}=require('../controllers/beneficiario')
+const {getBeneficiarios, postBeneficiario}=require('../controllers/beneficiario');
+const { validarCampos } = require('../middlewares/validar_campos');
 const router=Router()
 
 
@@ -13,7 +14,9 @@ router.post('/',[
     check('nombre','El nombre no puede estar vacio').notEmpty(),
     check('nombre','El nombre no puede ser un numero').isString(),
     check('password','El password deve incluir mayusculas,numeros y caracteres especiales').isStrongPassword(),
-    check('tarjeta','La tarjeta no es valida').isLength(16)
+    check('tarjeta','La tarjeta no es valida').isLength(16),
+    validarCampos,
+    validarToken
 ],postBeneficiario)
 
 module.exports=router
